84518bf68512eb78d1596f9fba977afe4150eb4d
mbaker341997 <mbaker341997@gmail.com>
2023-07-29 22:35:21 -0500

    Integrate openAPI and swagger ui

--- src/main/java/com/kinnock/musicdiary/utils/DateTimeUtils.java
+++ src/main/java/com/kinnock/musicdiary/utils/DateTimeUtils.java
@@ -1,5 +1,5 @@
 package com.kinnock.musicdiary.utils;
 
 public class DateTimeUtils {
-  public static final String LOCAL_DATE_FORMAT = "dd-MM-yyyy";
+  public static final String LOCAL_DATE_FORMAT = "yyyy-MM-dd";
 }
